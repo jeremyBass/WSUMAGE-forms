3b9477755fee799f41e8c9d2bb4c9abcc3b6d0e8
jeremyBass <jeremybass@cableone.net>
2014-03-04 08:39:48 -0800

    sets up the elements

---
+++ app/code/community/Wsu/WebForms/Block/Adminhtml/Element/Image.php
@@ -0,0 +1,19 @@
+<?php
+class Wsu_WebForms_Block_Adminhtml_Element_Image extends Wsu_WebForms_Block_Adminhtml_Element_File {
+	protected function _getPreviewHtml() {
+		$html = '';
+		if ($this->getData('result_id')) {
+			$result    = Mage::getModel('webforms/results')->load($this->getData('result_id'));
+			$field_id  = $this->getData('field_id');
+			$value     = $this->getData('value');
+			$thumbnail = $result->getThumbnail($field_id, $value, 100);
+			if ($thumbnail) {
+				$html .= '<div><img src="' . $thumbnail . '"/></div>';
+			}
+			if ($value) {
+				$html .= '<nobr><a href="' . $result->getDownloadLink($field_id, $value) . '">' . $value . '</a> <small>[' . $result->getFileSizeText($field_id, $value) . ']</small></nobr><br/>';
+			}
+		}
+		return $html;
+	}
+}
