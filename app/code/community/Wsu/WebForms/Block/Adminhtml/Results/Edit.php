be2ce94f164ec444e1978585fd6993d3fe45c7bf
jeremyBass <jeremybass@cableone.net>
2014-03-04 08:33:41 -0800

    sets out the result pages and grids

---
+++ app/code/community/Wsu/WebForms/Block/Adminhtml/Results/Edit.php
@@ -0,0 +1,56 @@
+<?php
+class Wsu_WebForms_Block_Adminhtml_Results_Edit extends Mage_Adminhtml_Block_Widget_Form_Container {
+	protected function _prepareLayout() {
+		parent::_prepareLayout();
+		if (Mage::getSingleton('cms/wysiwyg_config')->isEnabled()) {
+			$this->getLayout()->getBlock('head')->setCanLoadTinyMce(true);
+			$this->_formScripts[] = Mage::helper('webforms')->editorConfig();
+		}
+		// add scripts
+		$js = $this->getLayout()->createBlock('core/template', 'webforms_js', array(
+			'template' => 'webforms/logic.phtml',
+			'result' => Mage::registry('result'),
+			'webform' => Mage::registry('webform')
+		));
+		$this->getLayout()->getBlock('content')->append($js);
+	}
+	public function __construct() {
+		$result = Mage::getModel('webforms/results');
+		$id     = $this->getRequest()->getParam('id');
+		if ($id) {
+			$result->load($id);
+		}
+		Mage::register('result', $result);
+		$webform = Mage::getModel('webforms/webforms')->setStoreId($result->getStoreId())->load($result->getWebformId());
+		Mage::register('webform', $webform);
+		parent::__construct();
+		$this->_objectId   = 'id';
+		$this->_blockGroup = 'webforms';
+		$this->_controller = 'adminhtml_results';
+		$this->_addButton('reply', array(
+			'label' => Mage::helper('webforms')->__('Reply'),
+			'onclick' => 'setLocation(\'' . $this->getUrl('*/*/reply', array(
+				'_current' => true,
+				'webform_id' => $result->getWebformId()
+			)) . '\')'
+		));
+		$this->_addButton('saveandcontinue', array(
+			'label' => Mage::helper('adminhtml')->__('Save And Continue Edit'),
+			'onclick' => "$('saveandcontinue').value = true; editForm.submit()",
+			'class' => 'save'
+		), -100);
+	}
+	public function getSaveUrl() {
+		return $this->getUrl('*/adminhtml_results/save');
+	}
+	public function getBackUrl() {
+		return $this->getUrl('*/adminhtml_results/index', array(
+			'webform_id' => Mage::registry('result')->getWebformId()
+		));
+	}
+	public function getHeaderText() {
+		if (!is_null(Mage::registry('result')->getId())) {
+			return Mage::helper('webforms')->__("Result # %s | %s", Mage::registry('result')->getId(), Mage::helper('core')->formatDate(Mage::registry('result')->getCreatedTime(), Mage_Core_Model_Locale::FORMAT_TYPE_MEDIUM, true));
+		}
+	}
+}
