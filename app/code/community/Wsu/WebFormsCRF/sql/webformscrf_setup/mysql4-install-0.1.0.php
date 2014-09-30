d0cb3bf685f05fdfcff56c87895db0bae1f7e113
Jeremy Bass <jeremybass@cableone.net>
2014-09-30 14:19:10 -0700

    small clean up since i was in there

--- app/code/community/Wsu/WebFormsCRF/sql/webformscrf_setup/mysql4-install-0.1.0.php
+++ app/code/community/Wsu/WebFormsCRF/sql/webformscrf_setup/mysql4-install-0.1.0.php
@@ -3,10 +3,6 @@ $installer = $this;
 
 $installer->startSetup();
 
-$installer->run("
-ALTER TABLE  `{$this->getTable('customer/customer_group')}` ADD `webform_id` INT( 10 ) NOT NULL DEFAULT '0';
-");
-
 $webforms_table = $this->getTable('webforms/webforms');
 
 $installer->run("
@@ -16,10 +12,12 @@ ALTER TABLE  `{$webforms_table}` ADD  `crf_account_frontend`  tinyint(1) NOT NUL
 ALTER TABLE  `{$webforms_table}` ADD  `crf_account_group_serialized`  text NOT NULL;
 ");
 
-
-
+$customerGroup_table = $this->getTable('customer/customer_group');
 $installer->run("
-ALTER TABLE  `{$this->getTable('customer/customer_group')}` ADD `crf_activation_status` INT( 10 ) NOT NULL DEFAULT '1';
+ALTER TABLE `{$customerGroup_table}` ADD `webform_id` INT( 10 ) NOT NULL DEFAULT '0';
+");
+$installer->run("
+ALTER TABLE `{$customerGroup_table}` ADD `crf_activation_status` INT( 10 ) NOT NULL DEFAULT '1';
 ");
 
 
